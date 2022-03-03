f64ad1c0b4e8203eb5687292a570acb52b861df9
Opilane TTHK <opilane@tthk.ee>
2022-03-03 11:44:27 +0200

    urrrrr

---
+++ Bitcoin calculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bitcoin_calculator_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
